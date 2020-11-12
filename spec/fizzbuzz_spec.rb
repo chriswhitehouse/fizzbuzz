522b68f6604233dae43674513317f4040f5b2a72
AJ <aj.monty@gmail.com>
2020-11-12 13:51:18 +0000

    added spec to check multiple of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,15 +5,15 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(3)).to eq 'fizz'
     end
 
-    # it 'returns "fizz" when passed 9' do
-    #    expect(fizzbuzz(9)).to eq 'fizz'
-    # end
+    it 'returns "fizz" when passed 9' do
+       expect(fizzbuzz(9)).to eq 'fizz'
+    end
 
     it 'returns "buzz" when passed 5' do
         expect(fizzbuzz(5)).to eq 'buzz'
     end
 
-    # it 'returns 4 when passed 4' do
+    #it 'returns number when passed number not 3 or 5' do
     #    expect(fizzbuzz(4)).to eq 4
-    # end
+    #end
 end
