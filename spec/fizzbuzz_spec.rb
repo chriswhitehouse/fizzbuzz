39aa98bdafab85f1bc2efef55029d41aac722417
Chris Whitehouse <chris.whitehouse@hotmail.com>
2020-11-12 13:53:58 +0000

    added another failing spec for multiples of 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,6 +13,10 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(5)).to eq 'buzz'
     end
 
+    it 'returns "buzz" when passed 10' do
+        expect(fizzbuzz(10)).to eq 'buzz'
+    end
+
     #it 'returns number when passed number not 3 or 5' do
     #    expect(fizzbuzz(4)).to eq 4
     #end
